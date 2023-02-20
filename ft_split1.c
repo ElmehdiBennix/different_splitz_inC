ccf1bddafbaa80c0efefd96d1973647ce5a3e654
El Mehdi Bennix <ebennix@e2r2p11.1337.ma>
2023-02-20 23:01:53 +0100

    all splits i coded

---
+++ ft_split1.c
@@ -0,0 +1,107 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split1.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ebennix <ebennix@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/30 03:11:48 by ebennix           #+#    #+#             */
+/*   Updated: 2023/02/20 22:59:25 by ebennix          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct t_vb
+{
+	char	**tstr;
+	int		i;
+	int		j;
+	int		k;
+} s_vb;
+
+static int	count_word(char const *s, char c)
+{
+	unsigned int	i ;
+	unsigned int	word ;
+
+	i = 0;
+	word = 0;
+	while (s[i] != '\0')
+	{
+		while (s[i] == c)
+			i++;
+		if (s[i] != '\0')
+		{
+			while (s[i] != '\0' && s[i] != c)
+				i++;
+			word++;
+		}
+	}
+	return (word);
+}
+
+static int	len_word(char const *s, int i, char c)
+{
+	int	j;
+
+	j = 0;
+	while (s[i] != '\0' && s[i] != c)
+	{
+		i++;
+		j++;
+	}
+	return (j);
+}
+
+static	char **set_data(int size)
+{
+	char **tab;
+
+	tab = NULL;
+	tab = (char **)malloc(size * sizeof(char *));
+	if (!tab)
+		return (NULL);
+	return (tab);
+}
+
+static void	ft_clean(char **tstr)
+{
+	while (*tstr)
+	{
+		free(*tstr);
+		(*tstr)++;
+	}
+	free(tstr);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	s_vb vb;
+
+	ft_bzero(&vb , sizeof(s_vb));
+	if (!s)
+		return (NULL);
+	vb.tstr = set_data((count_word(s, c) + 1));
+	if (!vb.tstr)
+		return (NULL);
+	while (s[vb.i] != '\0')
+	{
+		while (s[vb.i] == c)
+			vb.i++;
+		if (s[vb.i] != '\0')
+		{
+			vb.k = 0;
+			vb.tstr[vb.j] = (char *)malloc((
+				len_word(s, vb.i, c) + 1) * sizeof(char));
+			if (!vb.tstr)
+				return (ft_clean(vb.tstr), NULL);
+			while (s[vb.i] != '\0' && s[vb.i] != c)
+				vb.tstr[vb.j][vb.k++] = s[vb.i++];
+			vb.tstr[vb.j++][vb.k] = '\0';
+		}
+	}
+	return (vb.tstr[vb.j] = NULL, vb.tstr);
+}
---
+++ ft_split2.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split2.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ebennix <ebennix@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/07 01:02:26 by ebennix           #+#    #+#             */
+/*   Updated: 2023/02/20 23:00:28 by ebennix          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *ft_strdup(const char *s)
+{
+    size_t i = 0;
+    char *str;
+    
+    str=(char *)malloc((ft_strlen(s)+1)*sizeof(char));
+    if (!str)
+        return NULL;
+    while (s[i])
+    {
+        str[i]=s[i];
+        i++;
+    }
+    str[i]='\0';
+    return str;
+}
+
+char** ft_split(const char *str, char delimiter)
+{
+    char *tmp = ft_strdup(str);
+    char *p = tmp;
+    char **result;
+    int count = 0;
+    int i;
+
+    while (*p) 
+    {
+        while (*p == delimiter)
+            p++;
+        if(*p)
+        {
+            while (*p != '\0' && *p != delimiter) 
+                p++;
+            count++;
+        }
+    } 
+    result = (char **)malloc((count + 1) * sizeof(char *));
+    if(result)
+        return(NULL);
+    i = 0;
+    count = 0;
+    p = tmp;
+    while (*p)
+    {
+        while (*p == delimiter)
+            p++;
+        if (*p)
+        {
+            while (*p && *p != delimiter)
+            {
+                i++;
+                p++;
+            }
+            result[count] = p - i;
+            *p = '\0';
+            count++;
+            p++;
+            i=0;
+        }
+    }
+    free(tmp);
+    result[count] = NULL;
+    return result;
+}
